3ec6103cc4a5dfd2fe793539b8f6d3fa141a38ab
Kurt James Clausen <kurtjclausen@gmail.com>
2023-06-12 09:01:47 -0700

    algorithms

--- week2_day1.js
+++ week2_day1.js
@@ -45,3 +45,27 @@ function makeFrequencyTable(arr) {
 console.log(makeFrequencyTable(arr1));
 console.log(makeFrequencyTable(arr2));
 console.log(makeFrequencyTable(arr3));
+
+// function makeFrequencyTable(array) {
+
+//     frequencyTable = {};
+
+//     const setStrings = new Set(array);
+
+//     const keys = Array.from(setStrings);
+        
+//     for (let j = 0; j < keys.length; ++ j){
+
+//         let key = keys[j];
+//         frequencyTable[key] = 0;
+
+//         for (let i = 0; i < array.length; ++ i) {
+//             if (array[i] === key) {
+//                 frequencyTable[key]++;
+//             }
+//         }
+//     }    return frequencyTable;
+// }
+
+// console.log(makeFrequencyTable(["", "b", "c", "c"]));
+// console.log(makeFrequencyTable([]));
